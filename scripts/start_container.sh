21320704f35a15f5fd236eea788163c64d573be1
Babu99525923 <57138810+Babu99525923@users.noreply.github.com>
2024-11-13 18:54:05 +0100

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull babu99525923/simple-python-app
+docker pull babu99525923/simple-python-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 babu99525923/simple-python-app
+docker run -d -p 5000:5000 babu99525923/simple-python-app:latest
